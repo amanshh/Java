6b26ed59b4700985cdf48f446acbdc0f9c6ae7a7
Aman Singh <146648263+amanshh@users.noreply.github.com>
2024-04-03 23:21:04 +0000

    Method

---
+++ Method/Advantages_of_function.java
@@ -0,0 +1,17 @@
+package Method;
+
+public class Advantages_of_function {
+    public static void main(String[] args) {
+// 2 Major Advantages of functions
+//    Code     Modularity (convert unstructured into structure style) ===> through which able to organise different different functionalities within Application
+//     Code    Reusability ==> ensure redundancy(duplicate code) of application is drastically reduced
+
+//        Moduler/ oraganized means seprate sections for all of the stuffs.
+
+//        Function VS Loops
+//  A function can be defined once and can be called any number of times by passing different arguments.
+
+//        Loops---> if the task is completed multiple times at same place.
+//        function---> if the task is being repeated multiple times but a different instances in your application.
+    }
+}
---
+++ Method/Syntax_of_function.java
@@ -0,0 +1,33 @@
+package Method;
+
+public class Syntax_Of_function {
+    public static void main(String[] args) {
+//        A function must have following 4 components:
+//        NAME of the function
+//        INPUT to the function(*Optional)
+//        ACTIVITY performed by the function
+//        OUTPUT form the function
+
+//         function Defination
+
+//        name(input)
+//        {
+//            activity or task
+//        }
+
+//        OR
+
+//        return_type name(parameters)
+//        {
+            // body of function
+//        }
+
+//        FUNCTION INVOCATION
+//        name(arguments);
+
+//        difference between arguments and function
+//        parameters ---> means the variable that are defined in function defination
+
+//        arguments ---> means the Actual data/hard code value that are going to input in function that would be collected by parameters
+    }
+}
